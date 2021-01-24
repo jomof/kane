c45aaffd07e621f7d211ddaec9bd1a1adb7c719e
Jomo Fisher <jomof@google.com>
2021-01-23 18:49:25 -0800

    Add row name to html output

--- src/main/kotlin/com/github/jomof/kane/sheet/Html.kt
+++ src/main/kotlin/com/github/jomof/kane/sheet/Html.kt
@@ -10,17 +10,17 @@ import com.github.jomof.kane.indexToColumnName
 val Sheet.html: String
     get() {
         val sb = StringBuilder()
-        sb.append(
-            """
-        <link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css\">
-          
-        <script type=\"text/javascript\" charset=\"utf8\" src=\"https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js\">
-        ${'$'}(document).ready( function () {
-            ${'$'}('#table_id').DataTable();
-        } );
-        </script>
-    """.trimIndent()
-        )
+//        sb.append(
+//            """
+//        <link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css\">
+//
+//        <script type=\"text/javascript\" charset=\"utf8\" src=\"https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js\">
+//        ${'$'}(document).ready( function () {
+//            ${'$'}('#table_id').DataTable();
+//        } );
+//        </script>
+//    """.trimIndent()
+//        )
 
         fun colName(column: Int) = columnDescriptors[column]?.name ?: indexToColumnName(column)
         fun rowName(row: Int) = rowDescriptors[row]?.name ?: "$row"
