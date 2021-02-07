9983e0a5711330b298f8e75b8f5bd1fd3a27b17c
Jomo Fisher <jomof@google.com>
2021-02-07 09:31:21 -0800

    lets_plot figures generation

--- src/test/kotlin/com/github/jomof/kane/FiguresTest.kt
+++ src/test/kotlin/com/github/jomof/kane/FiguresTest.kt
@@ -20,20 +20,8 @@ class FiguresTest {
             val cond by List(200) { "A" } + List(200) { "B" }
             listOf(rating, cond)
         }
-        sheet.head().assertString(
-            """
-                  rating [A] cond [B] 
-                  ---------- -------- 
-                1    0.84521        A 
-                2    0.91288        A 
-                3   -0.28708        A 
-                4    0.75186        A 
-                5    1.33547        A 
-            """.trimIndent()
-        )
 
-        val map = sheet.toMap()
-        var p: Plot = lets_plot(map)
+        var p: Plot = lets_plot(sheet.toMap())
 
         p += geom_density(color = "dark_green", alpha = .3) { x = "rating"; fill = "cond" }
         p += ggsize(500, 250)
