f9c0cb122bd190edb0cb17fa2a4def28af0522eb
Jomo Fisher <jomof@google.com>
2021-02-20 10:34:25 -0800

    Some work on Sequence<Row>

--- src/main/kotlin/com/github/jomof/kane/OperatorGet.kt
+++ src/main/kotlin/com/github/jomof/kane/OperatorGet.kt
@@ -3,21 +3,6 @@ package com.github.jomof.kane
 import com.github.jomof.kane.impl.*
 import com.github.jomof.kane.impl.sheet.*
 
-/**
- * Return a subsection of a sheet.
- */
-//operator fun Sheet.get(vararg ranges: String): Sheet {
-//    val first = ranges[0]
-//    if (ranges.size == 1 && looksLikeCellName(first)) {
-//        val sheet = this
-//        return sheetOf {
-//            listOf(ExogenousSheetScalar(cellNameToCoordinate(first), sheet).toNamed(coordinate(0, 0)))
-//        }
-//    }
-//    val asColumnIndices = tryConvertToColumnIndex(ranges.toList())
-//    if (asColumnIndices != null) return ordinalColumns(asColumnIndices)
-//    error("Couldn't get sheet subset for ${ranges.joinToString(",")}")
-//}
 
 /**
  * Return a single cell from a Sheet
@@ -29,6 +14,9 @@ operator fun Sheet.get(column: Int, row: Int): Expr {
 operator fun MatrixVariable.get(column: Int, row: Int): MatrixVariableElement = getMatrixElement(column, row)
 operator fun MatrixExpr.get(column: Int, row: Int): ScalarExpr = getMatrixElement(column, row)
 
+/**
+ * Return a subsection of a sheet.
+ */
 operator fun Sequence<Row>.get(vararg ranges: String): Sequence<Row> {
     val first = ranges[0]
     if (ranges.size == 1 && looksLikeCellName(first)) {
